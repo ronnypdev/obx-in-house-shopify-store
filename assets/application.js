fbb0d21992bef7d4ee116f9e7de8696437dc0456
Ronny Perez <ronnypdev@gmail.com>
2020-04-06 01:10:15 -0400

    Validating or Invalidating the Add to Cart Form

--- assets/application.js
+++ assets/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Currency Global Variables
   let moneySpanSelector = 'span.money';
   let currencyPickerSelector = '[name=currencies]';
@@ -6,16 +6,14 @@ $(document).ready(function() {
   let currencyNoteSelector = '.js-cart-currency-note';
 
   let currencyPicker = {
-    loadCurrency: function() {
+    loadCurrency: function () {
       /* Fix for customer account pages */
-      $(moneySpanSelector + ' ' + moneySpanSelector).each(function() {
-        $(this)
-          .parents(moneySpanSelector)
-          .removeClass('money');
+      $(moneySpanSelector + ' ' + moneySpanSelector).each(function () {
+        $(this).parents(moneySpanSelector).removeClass('money');
       });
 
       /* Saving the current price */
-      $(moneySpanSelector).each(function() {
+      $(moneySpanSelector).each(function () {
         $(this).attr('data-currency-' + shopCurrency, $(this).html());
       });
 
@@ -43,7 +41,7 @@ $(document).ready(function() {
       }
       currencyPicker.setCurrencyText();
     },
-    onCurrencyChanged: function(event) {
+    onCurrencyChanged: function (event) {
       let newCurrency = $(this).val();
       let $otherPickers = $(currencyPickerSelector).not($(this));
       Currency.convertAll(Currency.currentCurrency, newCurrency);
@@ -53,7 +51,7 @@ $(document).ready(function() {
         $otherPickers.val(newCurrency);
       }
     },
-    setCurrencyText: function(newCurrency = Currency.currentCurrency) {
+    setCurrencyText: function (newCurrency = Currency.currentCurrency) {
       let $activeCurrency = $(activeCurrencySelector);
       let $currencyNote = $(currencyNoteSelector);
 
@@ -69,12 +67,12 @@ $(document).ready(function() {
         }
       }
     },
-    onMoneySpanAdded: function() {
+    onMoneySpanAdded: function () {
       Currency.convertAll(shopCurrency, Currency.currentCurrency);
       currencyPicker.setCurrencyText();
     },
 
-    init: function() {
+    init: function () {
       if (showMultipleCurrencies !== true) {
         return false;
       }
@@ -90,4 +88,78 @@ $(document).ready(function() {
   };
 
   currencyPicker.init();
+
+  // Add to Cart Form
+
+  let addToCartFormSelector = '#add-to-cart-form';
+  let productOptionSelector = addToCartFormSelector + ' [name*=option]';
+
+  let productForm = {
+    onProductOptionChanged: function (event) {
+      let $form = $(this).closest(addToCartFormSelector);
+      let selectedVariant = productForm.getActiveVariant($form);
+
+      $form.trigger('form:change', [selectedVariant]);
+    },
+    getActiveVariant: function ($form) {
+      let variants = JSON.parse(
+        decodeURIComponent($form.attr('data-variants'))
+      );
+      let formData = $form.serializeArray();
+      let formOptions = {
+        option1: null,
+        option2: null,
+        option3: null
+      };
+
+      selectedVariant = null;
+
+      $.each(formData, function (index, item) {
+        if (item.name.indexOf('option') !== -1) {
+          formOptions[item.name] = item.value;
+        }
+      });
+
+      $.each(variants, function (index, variant) {
+        if (
+          variant.option1 === formOptions.option1 &&
+          variant.option2 === formOptions.option2 &&
+          variant.option3 === formOptions.option3
+        ) {
+          selectedVariant = variant;
+          return false;
+        }
+      });
+      return selectedVariant;
+    },
+    validate: function (event, selectedVariant) {
+      let $form = $(this);
+      let hasVariant = selectedVariant !== null;
+      let canAddToCart = hasVariant && selectedVariant.inventory_quantity > 0;
+      let $id = $form.find('.js-variant-id');
+      let $addToCartButton = $form.find('#add-to-cart-button');
+
+      if (canAddToCart) {
+        $id.val(selectedVariant.id);
+        $addToCartButton.prop('disabled', false);
+      } else {
+        $id.val();
+        $addToCartButton.prop('disabled', true);
+      }
+    },
+    init: function () {
+      $(document).on(
+        'change',
+        productOptionSelector,
+        productForm.onProductOptionChanged
+      );
+      $(document).on(
+        'form:change',
+        addToCartFormSelector,
+        productForm.validate
+      );
+    }
+  };
+
+  productForm.init();
 });
